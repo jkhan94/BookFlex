b91617c7844b891ccfb0c5fa4f369aa325e892b8
HongYongGuen <pioneer8919@naver.com>
2024-07-19 14:32:53 +0900

    first commit

---
+++ src/main/java/com/sparta/bookflex/BookFlexApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.bookflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookFlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookFlexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/bookflex/BookFlexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.bookflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookFlexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
