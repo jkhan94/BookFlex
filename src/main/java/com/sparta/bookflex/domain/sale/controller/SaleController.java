ac73558ff710373a911b3a141f6b673e63ca6533
HongYongGuen <pioneer8919@naver.com>
2024-07-22 23:06:22 +0900

    [#5] feat : Sale 전체 등록 및 조회 기능 추가
    - Sale 전체 등록 및 조회 기능 구현
    - SaleController, SaleService 메소드 추가

--- src/main/java/com/sparta/bookflex/domain/sale/controller/SaleController.java
+++ src/main/java/com/sparta/bookflex/domain/sale/controller/SaleController.java
@@ -2,22 +2,22 @@ package com.sparta.bookflex.domain.sale.controller;
 
 import com.sparta.bookflex.common.dto.CommonDto;
 import com.sparta.bookflex.common.security.UserDetailsImpl;
-import com.sparta.bookflex.domain.sale.dto.SaleCreateReqDto;
-import com.sparta.bookflex.domain.sale.dto.SaleResDto;
+import com.sparta.bookflex.domain.sale.dto.OrderRequestDto;
+import com.sparta.bookflex.domain.sale.dto.SaleRequestDto;
+import com.sparta.bookflex.domain.sale.dto.SaleResponseDto;
 import com.sparta.bookflex.domain.sale.service.SaleService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("api/sales")
 public class SaleController {
 
-    private SaleService saleService;
+    private final SaleService saleService;
 
     public SaleController(SaleService saleService) {
         this.saleService = saleService;
@@ -25,36 +25,45 @@ public class SaleController {
 
     @PostMapping()
     public ResponseEntity<CommonDto<Void>> createSale(
-            @RequestBody SaleCreateReqDto saleCreateReqDto
-            //,@AuthenticationPrincipal UserDetailsImpl userDetails
-            ) {
-        Long userId = 1L;
-        saleService.createSale(saleCreateReqDto, userId);
+            @RequestBody SaleRequestDto saleCreateReqDto
+            ,@AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        saleService.createSale(saleCreateReqDto, userDetails.getUser());
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new CommonDto<>(HttpStatus.CREATED.value(), "주문이 완료되었습니다.", null));
     }
 
-    @GetMapping("/{saleId")
-    public ResponseEntity<CommonDto<SaleResDto>> getSale(
+    @PostMapping("/orderplace")
+    public ResponseEntity<CommonDto<Void>> createMultipleSales(
+            @RequestBody OrderRequestDto orderRequestDto,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        saleService.createMultipleSales(orderRequestDto, userDetails.getUser());
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new CommonDto<>(HttpStatus.CREATED.value(), "주문이 완료되었습니다.", null));
+    }
+
+    @GetMapping("/{saleId}")
+    public ResponseEntity<CommonDto<SaleResponseDto>> getSale(
             @PathVariable Long saleId
-            //,@AuthenticationPrincipal UserDetailsImpl userDetails
-    ) {
-        Long userId = 1L;
-        SaleResDto saleResDto = saleService.getSale(saleId, userId);
+            ,@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        SaleResponseDto saleResDto = saleService.findSaleById(saleId, userDetails.getUser());
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(new CommonDto<>(HttpStatus.OK.value(), "주문 내역을 조회했습니다.", saleResDto));
     }
 
-    @DeleteMapping("/{saleId}")
-    public ResponseEntity<CommonDto<Void>> deleteSale(
-            @PathVariable Long saleId
-            //,@AuthenticationPrincipal UserDetailsImpl userDetails
-    ) {
-        Long userId = 1L;
-        saleService.deleteSale(saleId, userId);
+    @GetMapping("")
+    public ResponseEntity<CommonDto<Page<SaleResponseDto>>> getAllSalesForUser(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            Pageable pageable) {
+        Page<SaleResponseDto> saleResponseDtoList = saleService.getAllSalesForUser(userDetails.getUser(), pageable);
         return ResponseEntity
-                .status(HttpStatus.NO_CONTENT).body(null);
+                .status(HttpStatus.OK)
+                .body(new CommonDto<>(HttpStatus.OK.value(), "주문 목록을 조회했습니다.", saleResponseDtoList));
     }
+
+
 }
--- src/main/java/com/sparta/bookflex/domain/sale/service/SaleService.java
+++ src/main/java/com/sparta/bookflex/domain/sale/service/SaleService.java
@@ -1,93 +1,93 @@
 package com.sparta.bookflex.domain.sale.service;
 
 import com.sparta.bookflex.domain.book.entity.Book;
-import com.sparta.bookflex.domain.book.repository.BookRepository;
-import com.sparta.bookflex.domain.sale.dto.SaleCreateReqDto;
-import com.sparta.bookflex.domain.sale.dto.SaleResDto;
+import com.sparta.bookflex.domain.book.service.BookService;
+import com.sparta.bookflex.domain.sale.Enum.SaleState;
+import com.sparta.bookflex.domain.sale.dto.OrderRequestDto;
+import com.sparta.bookflex.domain.sale.dto.SaleRequestDto;
+import com.sparta.bookflex.domain.sale.dto.SaleResponseDto;
 import com.sparta.bookflex.domain.sale.entity.Sale;
 import com.sparta.bookflex.domain.sale.repository.SaleRepository;
 import com.sparta.bookflex.domain.user.entity.User;
-import com.sparta.bookflex.domain.user.repository.UserRepository;
+import com.sparta.bookflex.domain.user.service.AuthService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class SaleService {
     private final SaleRepository saleRepository;
-    private final UserRepository userRepository;
-    private final BookRepository bookRepository;
+    private final AuthService authService;
+    private final BookService bookService;
 
-    public SaleService(SaleRepository saleRepository,
-                       UserRepository userRepository,
-                       BookRepository bookRepository) {
-        this.userRepository = userRepository;
-        this.bookRepository = bookRepository;
+
+    public SaleService(SaleRepository saleRepository, AuthService authService, BookService bookService) {
         this.saleRepository = saleRepository;
+        this.authService = authService;
+        this.bookService = bookService;
     }
 
-    public void createSale(SaleCreateReqDto saleCreateReqDto, Long userId) {
-        User user = getUser(userId);
+    private Book getBook(Long bookId) {
+        return bookService.getBookByBookId(bookId);
+    }
+
+    private Sale getSale(Long saleId) {
+        return saleRepository.findById(saleId).orElseThrow(() -> new IllegalArgumentException("해당 상품이 존재하지 않습니다."));
+    }
+
+    @Transactional
+    public void createSale(SaleRequestDto saleCreateReqDto, User user) {
+
         Book book = getBook(saleCreateReqDto.getBookId());
         Sale sale = Sale.builder()
                 .quantity(saleCreateReqDto.getQuantity())
+                .status(SaleState.PENDING_PAYMENT)
                 .book(book)
                 .user(user)
                 .build();
         saleRepository.save(sale);
     }
 
+    @Transactional
+    public void createMultipleSales(OrderRequestDto orderRequestDto, User user) {
 
-    public SaleResDto getSale(Long saleId, Long userId) {
-        if(Boolean.FALSE.equals(existUser(userId))) {
-            throw new IllegalArgumentException("존재하지 않는 유저입니다.");
+        for (OrderRequestDto.OrderItemDto orderItemDto : orderRequestDto.getItems()) {
+            Book book = getBook(orderItemDto.getBookId());
+            Sale sale = Sale.builder()
+                    .quantity(orderItemDto.getQuantity())
+                    .status(SaleState.PENDING_PAYMENT)
+                    .book(book)
+                    .user(user)
+                    .build();
+            saleRepository.save(sale);
         }
+    }
+
+    public SaleResponseDto findSaleById(Long saleId, User user) {
         Sale sale = getSale(saleId);
-        return SaleResDto.builder()
+        return SaleResponseDto.builder()
                 .saleId(sale.getId())
                 .bookName(sale.getBook().getBookName())
                 .price(sale.getBook().getPrice())
                 .quantity(sale.getQuantity())
-                .status(sale.getStatus())
+                .status(sale.getStatus().getDesscription())
                 .createdAt(sale.getCreatedAt())
                 .build();
     }
 
-    public void deleteSale(Long saleId, Long userId) {
-        if(Boolean.FALSE.equals(existSale(saleId))) {
-            throw new IllegalArgumentException("존재하지 않는 주문입니다.");
-        }
-        if(Boolean.FALSE.equals(existUser(userId))) {
-            throw new IllegalArgumentException("존재하지 않는 유저입니다.");
-        }
-        Sale sale = getSale(saleId);
-        saleRepository.delete(sale);
+
+    public Page<SaleResponseDto> getAllSalesForUser(User user, Pageable pageable) {
+        return saleRepository.findAllSalesByUser(user, pageable).map(
+                sale -> SaleResponseDto.builder()
+                        .saleId(sale.getId())
+                        .bookName(sale.getBook().getBookName())
+                        .price(sale.getBook().getPrice())
+                        .quantity(sale.getQuantity())
+                        .status(sale.getStatus().getDesscription())
+                        .createdAt(sale.getCreatedAt())
+                        .build()
+        );
+
     }
-
-    private User getUser(Long userId) {
-        return userRepository.findById(userId).orElseThrow(
-                () -> new IllegalArgumentException("유저가 없습니다."));
-    }
-
-    private Book getBook(Long bookId) {
-        return bookRepository.findById(bookId).orElseThrow(
-                () -> new IllegalArgumentException("책이 없습니다."));
-    }
-
-    private Sale getSale(Long saleId) {
-        return saleRepository.findById(saleId).orElseThrow(
-                () -> new IllegalArgumentException("상품이 없습니다."));
-    }
-
-    private Boolean existSale(Long saleId) {
-        return saleRepository.existsById(saleId);
-    }
-
-    private Boolean existBook(Long bookId) {
-        return bookRepository.existsById(bookId);
-    }
-
-    private Boolean existUser(Long userId) {
-        return userRepository.existsById(userId);
-    }
-
-
 }
