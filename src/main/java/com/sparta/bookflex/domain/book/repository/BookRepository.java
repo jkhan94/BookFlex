e2344bc5c9183882bf3d301066ec633ffc2335c2
이종원 <ejong_1@naver.com>
2024-07-22 20:37:07 +0900

    [#6] update : bookName으로 상품 조회하는 메서드 추가, 메서드별 CommonDto massage 수정

--- src/main/java/com/sparta/bookflex/domain/book/repository/BookRepository.java
+++ src/main/java/com/sparta/bookflex/domain/book/repository/BookRepository.java
@@ -3,10 +3,11 @@ package com.sparta.bookflex.domain.book.repository;
 import com.sparta.bookflex.domain.book.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    Optional<Book> findByBookName(String bookName);
+    List<Book> findByBookName(String bookName);
 
 
 
