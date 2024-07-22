4ef8dbce97e6c1a8abb648512c62623cfdd58304
jkhan94 <hjkwork@naver.com>
2024-07-22 18:33:25 +0900

    del : 쿼리DSL 설정
    QueryDSLConfig.java : entityManager에 @PersistenceContext 추가

--- src/main/java/com/sparta/bookflex/common/config/QueryDSLConfig.java
+++ src/main/java/com/sparta/bookflex/common/config/QueryDSLConfig.java
@@ -2,6 +2,7 @@ package com.sparta.bookflex.common.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class QueryDSLConfig {
 
+    @PersistenceContext
     private final EntityManager entityManager;
 
     @Bean
