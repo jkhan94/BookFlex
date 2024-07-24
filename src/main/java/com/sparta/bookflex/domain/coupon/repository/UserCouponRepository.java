cff40f610419249c6ff84e907f2e28632a758e25
HongYongGuen <pioneer8919@naver.com>
2024-07-24 18:13:31 +0900

    [#36] fix : usercopupon 이동에 따른 문제 수정

--- src/main/java/com/sparta/bookflex/domain/coupon/repository/UserCouponRepository.java
+++ src/main/java/com/sparta/bookflex/domain/coupon/repository/UserCouponRepository.java
@@ -1,6 +1,6 @@
 package com.sparta.bookflex.domain.coupon.repository;
 
-import com.sparta.bookflex.domain.usercoupon.entity.UserCoupon;
+import com.sparta.bookflex.domain.coupon.entity.UserCoupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
