83b35c0acfe3643b9fae0607ed433a8ecc8c273c
HongYongGuen <pioneer8919@naver.com>
2024-07-24 22:57:11 +0900

    [#40] update : dto 수정에 따른 ordercreate 변경
    - createOrder postman test 완료
    - book 변경 해주는 부분 추가 필요!

--- src/main/java/com/sparta/bookflex/domain/orderbook/service/OrderBookService.java
+++ src/main/java/com/sparta/bookflex/domain/orderbook/service/OrderBookService.java
@@ -61,7 +61,7 @@ public class OrderBookService {
 
         for (OrderRequestDto.OrderItemDto item : orderRequestDto.getItems()) {
             Book book = getBook(item.getBookId());
-            BigDecimal price = book.getPrice();
+            BigDecimal price = item.getPrice();
             BigDecimal itemTotal = BigDecimal.valueOf(item.getQuantity()).multiply(price);
             total = total.add(itemTotal);
 
