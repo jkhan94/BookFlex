335518af5e8ad732468ecf194309e6f62ea60dc1
jkhan94 <hjkwork@naver.com>
2024-07-25 11:53:36 +0900

    refactor : 카테고리 컨트롤러
    -CategoryController
    1. 주석 제거
    2. 모든 카테고리 조회 API URL 변경

--- src/main/java/com/sparta/bookflex/domain/category/controller/CategoryController.java
+++ src/main/java/com/sparta/bookflex/domain/category/controller/CategoryController.java
@@ -4,10 +4,6 @@ import com.sparta.bookflex.common.aop.Envelop;
 import com.sparta.bookflex.domain.category.dto.CategoryAllResponseDto;
 import com.sparta.bookflex.domain.category.dto.CategoryNameResponseDto;
 import com.sparta.bookflex.domain.category.service.CategoryService;
-import com.sparta.bookflex.domain.user.entity.User;
-import com.sparta.bookflex.domain.user.enums.RoleType;
-import com.sparta.bookflex.domain.user.service.AuthService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,28 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-import static com.sparta.bookflex.common.exception.ErrorCode.USER_NOT_AUTHORIZED;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/categories")
 public class CategoryController {
     private final CategoryService categoryService;
 
-    /*모든 카테고리 목록 조회
-    GET /categories
-
-    응답DTO
-    statusCode	200
-    message	“모든 카테고리 조회에 성공하였습니다.”
-    data	{
-            categoryName	카테고리 이름
-            },
-            {
-            categoryName	카테고리 이름
-            }, …
-    * */
-    @GetMapping("/enum")
+    @GetMapping
     @Envelop("모든 카테고리 조회에 성공하였습니다.")
     public ResponseEntity<List<CategoryAllResponseDto>> getAllCategories() {
 
@@ -55,18 +36,6 @@ public class CategoryController {
         return ResponseEntity.status(HttpStatus.OK).body(responseList);
     }
 
-    /*서브 카테고리 목록 조회
-    GET /categories/sub
-
-    응답DTO
-    statusCode	200
-    message	“서브 카테고리 조회에 성공하였습니다”
-    data	{
-            categoryName	카테고리 이름
-            },
-            {
-            categoryName	카테고리 이름
-            }, …	*/
     @GetMapping("/sub")
     @Envelop("서브 카테고리 목록 조회에 성공하였습니다.")
     public ResponseEntity<List<CategoryNameResponseDto>> getAllSubCategories(@RequestParam(value = "mainCategory", defaultValue = "카테고리") String mainCategory) {
