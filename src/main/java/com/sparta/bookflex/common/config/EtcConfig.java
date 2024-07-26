647c8df7dfe6333acb486c0f0182a43cdb7939f8
jkhan94 <hjkwork@naver.com>
2024-07-26 18:17:56 +0900

    update : 설정 추가
    -build.gradle
    1. 문자열 난수 생성을 휘한 의존성 추가
    
    -EtcConfig
    1. 스케줄링 적용

--- src/main/java/com/sparta/bookflex/common/config/EtcConfig.java
+++ src/main/java/com/sparta/bookflex/common/config/EtcConfig.java
@@ -3,11 +3,13 @@ package com.sparta.bookflex.common.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableJpaAuditing
+@EnableScheduling
 public class EtcConfig {
 
     @Bean
