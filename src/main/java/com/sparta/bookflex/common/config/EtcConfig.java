422cef6b2558e1e950492bf98eca50000093d9fa
inho9979 <inho9979@naver.com>
2024-07-26 19:24:28 +0900

    ✨feat[#49]: 배송 시스템 틀 잡기 (dto, controller, Config 등)

--- src/main/java/com/sparta/bookflex/common/config/EtcConfig.java
+++ src/main/java/com/sparta/bookflex/common/config/EtcConfig.java
@@ -1,10 +1,14 @@
 package com.sparta.bookflex.common.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 @EnableJpaAuditing
@@ -13,5 +17,13 @@ public class EtcConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public RestTemplate RestTemplateService (RestTemplateBuilder builder) {
+        return builder
+            .setConnectTimeout(Duration.ofSeconds(5))
+            .setReadTimeout(Duration.ofSeconds(5))
+            .build();
     }
 }
--- src/main/java/com/sparta/bookflex/domain/shipment/entity/Shipment.java
+++ src/main/java/com/sparta/bookflex/domain/shipment/entity/Shipment.java
@@ -1,10 +1,8 @@
 package com.sparta.bookflex.domain.shipment.entity;
 
 import com.sparta.bookflex.common.utill.Timestamped;
-import com.sparta.bookflex.domain.orderbook.entity.OrderBook;
 import com.sparta.bookflex.domain.orderbook.entity.OrderItem;
-import com.sparta.bookflex.domain.sale.Enum.SaleState;
-import com.sparta.bookflex.domain.sale.entity.Sale;
+import com.sparta.bookflex.domain.shipment.enums.ShipmentEnum;
 import com.sparta.bookflex.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Builder;
@@ -31,8 +29,7 @@ public class Shipment extends Timestamped {
     private String carrier;
 
     @Column(name = "status",nullable = false)
-    private SaleState status;
-
+    private ShipmentEnum status;
 
     @Column(name = "shipped_at")
     private LocalDateTime shippedAt;
@@ -51,7 +48,7 @@ public class Shipment extends Timestamped {
     User user;
 
     @Builder
-    public Shipment(String trackingNumber, String carrier, SaleState status, LocalDateTime shippedAt, LocalDateTime deliveredAt, List<OrderItem> orderItemList,User user,String address) {
+    public Shipment(String trackingNumber, String carrier, ShipmentEnum status, LocalDateTime shippedAt, LocalDateTime deliveredAt, List<OrderItem> orderItemList,User user,String address) {
         this.trackingNumber = trackingNumber;
         this.carrier = carrier;
         this.status = status;
@@ -61,6 +58,4 @@ public class Shipment extends Timestamped {
         this.user = user;
         this.address = address;
     }
-
-
 }
