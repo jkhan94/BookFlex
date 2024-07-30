5736b310a53be59a73e5c0f3d8b5c725453bfb9f
jkhan94 <hjkwork@naver.com>
2024-07-30 20:00:28 +0900

    update : Qna 기능 추가 및 엔티티 수정
    엔티티 변경사항
    Coupon
    couponType, discountType, ValidityDays, EligibleGrade 추가
    
    userCoupon
    issuedAt, expirationDate 추가
    
    API 추가
    1. 발급 가능한 쿠폰 사용자 조회
    2. 쿠폰 종류 조회
    3. 할인 유형 조회
    4. 사용자 등급 조회
    
    스케줄러 추가
    1. 생일 쿠폰
    2. 등급 쿠폰

--- src/main/java/com/sparta/bookflex/domain/coupon/repository/UserCouponQRepositoryImpl.java
+++ src/main/java/com/sparta/bookflex/domain/coupon/repository/UserCouponQRepositoryImpl.java
@@ -2,15 +2,22 @@ package com.sparta.bookflex.domain.coupon.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sparta.bookflex.domain.coupon.entity.UserCoupon;
+import com.sparta.bookflex.domain.coupon.enums.CouponType;
+import com.sparta.bookflex.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.YearMonth;
 import java.util.List;
 
 import static com.sparta.bookflex.domain.coupon.entity.QCoupon.coupon;
 import static com.sparta.bookflex.domain.coupon.entity.QUserCoupon.userCoupon;
+import static com.sparta.bookflex.domain.user.entity.QUser.user;
 
 @RequiredArgsConstructor
 public class UserCouponQRepositoryImpl implements UserCouponQRepository {
@@ -33,5 +40,37 @@ public class UserCouponQRepositoryImpl implements UserCouponQRepository {
                 .fetch();
 
         return new PageImpl<>(result, pageable, count.size());
+    }
+
+    @Override
+    @Transactional
+    public void updateGradeCoupon() {
+        List<User> count = queryFactory.select(user).from(user).fetch();
+
+        LocalDate start = YearMonth.now().atDay(1);
+        LocalDate end = YearMonth.now().atEndOfMonth();
+
+        queryFactory.update(coupon)
+                .set(coupon.totalCount, Integer.MAX_VALUE)
+                .set(coupon.startDate, start.atStartOfDay())
+                .set(coupon.expirationDate, end.atTime(LocalTime.MAX))
+                .where(coupon.couponType.eq(CouponType.GRADE))
+                .execute();
+    }
+
+    @Override
+    @Transactional
+    public void updateBirthdayCoupon() {
+        List<User> count = queryFactory.select(user).from(user).where(user.birthDay.isNotNull()).fetch();
+
+        LocalDate start = YearMonth.now().atDay(1);
+        LocalDate end = YearMonth.now().atEndOfMonth();
+
+        queryFactory.update(coupon)
+                .set(coupon.totalCount, Integer.MAX_VALUE)
+                .set(coupon.startDate, start.atStartOfDay())
+                .set(coupon.expirationDate, end.atTime(LocalTime.MAX))
+                .where(coupon.couponType.eq(CouponType.BIRTHDAY))
+                .execute();
     }
 }
