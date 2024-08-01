6d81cb7eed78825c8077edc90ab87bc0423e26df
inho9979 <inho9979@naver.com>
2024-08-01 20:27:51 +0900

    ✨ feat[#88]: 머지후 삭제된것 추가

--- src/main/java/com/sparta/bookflex/domain/basket/repository/BasketItemRepository.java
+++ src/main/java/com/sparta/bookflex/domain/basket/repository/BasketItemRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface BasketItemRepository extends JpaRepository<BasketItem, Long> {
+    boolean existsByBookIdAndBasketId(Long bookId, Long basketId);
 
     Page<BasketItem> findAllByBasketId(Long basketId, Pageable pageable);
 
